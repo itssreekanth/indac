8e7e36cae611a7e2fafdb6b8afaa7e9a66ff878c
itssreekanth <69184624+itssreekanth@users.noreply.github.com>
2021-11-05 11:29:47 +0530

    Update speedometer1.py

--- digital_speedometer/speedometer1.py
+++ digital_speedometer/speedometer1.py
@@ -1,13 +1,13 @@
 from math import pi
 import numpy as np
 from time import sleep
-def velocity(t,r = 0.3):
+def velocity(t,r = 0.1):
     rps = 1/t                     #t : time taken for one revolution
-    circumference = 2*pi*r        #r : radius of the wheel
+    circumference = 2*pi*r        #r : distance of sensor from the centre of wheel
     v = rps*circumference         # gives the velocity in m/s
     return np.round(v*3.6,2)      # returns velocity in km/hr
-def distance(rev,r = 0.3):
-    return rev*2*pi*r
+def distance(rev,r = 0.3):        #r : radius of the wheel
+    return rev*2*pi*r             
 s = np.random.rand(10)            #randomly picks time values for n revolutions
 tot_dist = 0
 time = 0
